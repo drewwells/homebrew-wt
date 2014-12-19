a94a2f295357516be8f36f470110c83f5c5d6693
Drew Wells <drew.wells00@gmail.com>
2014-12-19 10:15:45 -0600

    Update to static building

--- wellington.rb
+++ wellington.rb
@@ -3,26 +3,54 @@ require "language/go"
 
 class Wellington < Formula
   homepage "https://github.com/wellington/wellington"
-  url "https://github.com/wellington/wellington/archive/f1ef09a7b86e5c96aabe52e76513baa02e929cc7.tar.gz"
-  sha1 "90dcae87a3dccd675505b2f23ad47b2ce31fa8fb"
+  url "https://github.com/wellington/wellington/archive/61a7f17b9612ec552a18d51c8e5dee307e323743.tar.gz"
+  #sha1 "c3abd876e209c00a7563866e07028db359a8d4f6"
   head "https://github.com/wellington/wellington.git"
 
-  option :cxx11
+  needs :cxx11
 
   depends_on "go" => :build
-  depends_on "pkgconfig" => :build
+  depends_on "pkg-config" => :build
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
 
   go_resource "github.com/wellington/spritewell" do
-    url "http://github.com/wellington/spritewell.git",
-      :revision => "3a43f26d94a6da8e40884d1edca0ff372ab7487d"
+    url "https://github.com/wellington/spritewell.git",
+        :revision => "3a43f26d94a6da8e40884d1edca0ff372ab7487d"
+    sha1 ""
+  end
+
+  go_resource "github.com/go-fsnotify/fsnotify" do
+    url "https://github.com/go-fsnotify/fsnotify.git",
+        :revision => "f582d920d11386e8ae15227bb5933a8f9b4c3dec"
+    sha1 "2bd8459d120337ad68cb119b6761130c2e07d053"
+  end
+
+  resource "github.com/sass/libsass" do
+    url "https://github.com/sass/libsass.git"
+    sha1 "705e6dc406229571ee992e83b797fb4abbecb826"
   end
 
   def install
-
+    resource("github.com/sass/libsass").stage {
+      ENV["LIBSASS_VERSION"]="705e6d"
+      system "autoreconf", "-fvi"
+      system "./configure", "--prefix=#{buildpath}/libsass",
+             "--enable-static",
+             "--disable-silent-rules",
+             "--disable-dependency-tracking"
+      system "make", "install"
+      # dylibs will be used if found, remove them to force static binding
+      rm Dir.glob("#{buildpath}/libsass/lib/*dylib")
+    }
+    # go_resource doesn't support gopkg, do it manually then symlink
+    mkdir_p buildpath/"src/gopkg.in"
+    ln_s buildpath/"src/github.com/go-fsnotify/fsnotify", buildpath/"src/gopkg.in/fsnotify.v1"
+    ENV.append_path "PKG_CONFIG_PATH", buildpath/"libsass/lib/pkgconfig"
     mkdir_p buildpath/"src/github.com/wellington"
     ln_s buildpath, buildpath/"src/github.com/wellington/wellington"
     Language::Go.stage_deps resources, buildpath/"src"
-    ENV["PKG_CONFIG_PATH"] = "/usr/local/lib/pkgconfig"
     ENV["GOPATH"] = buildpath
 
     system "go", "build", "-o", "dist/wt", "wt/main.go"
@@ -31,9 +59,13 @@ class Wellington < Formula
 
   test do
     path = testpath/"file.scss"
-    path.write "div { p { color: red; } }"
-    lines = `#{bin}/wt #{path}`
-    assert_equal(`div p {
-  color: red; }`, lines)
+    path.write "div { p { color: red; }}"
+    expected = <<-EOS.undent
+      /* line 6, stdin */
+      div p {
+        color: red; }
+    EOS
+    output = `#{bin}/wt #{path}`
+    assert_equal(expected, output)
   end
 end
